3de89362051b4892bef5a182375f49a0804f65ee
sbclassic <benjoye21@gmail.com>
2025-06-14 18:41:11 +0000

    Update downloads-data.js

--- downloads-data.js
+++ downloads-data.js
@@ -1,1 +1,14 @@
-const downloadData = [];
+const downloadData = [
+  {
+    book: "The Paradox of Passion",
+    format: "PDF",
+    timestamp: "2025-06-14T10:15:00Z",
+    ip: "102.66.35.1"
+  },
+  {
+    book: "What It Took",
+    format: "EPUB",
+    timestamp: "2025-06-14T11:01:12Z",
+    ip: "102.89.211.24"
+  }
+];
