fbcbae8ffb644a3afb19c993da81e4a41253afcd
sbclassic <benjoye21@gmail.com>
2025-07-08 22:13:06 +0000

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,7 +1,6 @@
 const admin = require("firebase-admin");
 
-// Parse the JSON from the environment variable
-const serviceAccount = JSON.parse(process.env.FIREBASE_KEY);
+const serviceAccount = JSON.parse(process.env.FIREBASE_CONFIG);
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
